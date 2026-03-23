ff5e5dcb615e818718df61cdec5e84153516da51
All users <All users@DESKTOP-7GRON3D>
2026-03-23 15:51:48 +0200

    Add project files.

---
+++ Models/BookingManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventease.Models
+{
+    public class BookingManager
+    {
+        public int BookingId { get; set; }
+        public string BookingName { get; set; }
+        public int VenueId { get; set; }
+        public int EventId {  get; set; }
+    }
+}
